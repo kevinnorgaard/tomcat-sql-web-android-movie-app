7d2fbc721edbc8b2629d8505064b40a91e2b1923
Kevin Norgaard <norgaark@uci.edu>
2020-06-05 21:46:37 -0700

    Updated README with demo URL

--- logs/log_processing.py
+++ logs/log_processing.py
@@ -1,14 +1,18 @@
-filename = "single-instance-http-10-nopooling.txt"
+filename = "gcp-scaled-http-10.txt"
 
 ts_sum = tj_sum = 0
 
 with open(filename) as f:
     content = f.readlines()
+    errors = 0
     for line in content:
-    	ts, tj = line.strip().split(",")
-    	ts_sum += int(ts)
-    	tj_sum += int(tj)
+    	try:
+    		ts, tj = line.strip().split(",")
+    		ts_sum += int(ts)
+    		tj_sum += int(tj)
+    	except:
+    		errors += 1
 
-    denom = len(content) * 10**6
+    denom = (len(content) - errors) * 10**6
     print("Average Search Servlet Time(ms): " + str(ts_sum / denom))
     print("Average JDBC Time(ms): " + str(tj_sum / denom))
